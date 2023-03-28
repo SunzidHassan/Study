b475c40438c4536c7286c2f79f182a16a840c736
Sunzid Hassan <sunzidhassan@gmail.com>
2023-03-27 23:05:45 -0500

    update

--- Coursework/521_Computer_Architecture/Class5/testq1t.py
+++ Coursework/521_Computer_Architecture/Class5/testq1t.py
@@ -1,36 +1,45 @@
 from bcpu import *
 
+r1 = 0
+r2 = 0
+# python function
 def Abs(inx:r1)->r2:
     outy:r2 = inx
     if inx < 0:
         outy = -inx
     return outy # asm will get the return value from r2
 
+# assembly function
 from testq1 import absf
 
-# testing
-if __name__ == '__main__':
-    # put all your testing code here
+# testing assembly to python
+if __name__ == '__main__': # put all your testing code in main()
+    
+    ## test case 1
     tot = 0
     testcase = -5
-    outp = Abs(testcase)
-    print("py our: ", outp)
-    setr(r1, testcase)
-    start(absf)
-    # printr(r2)
-    outa = getr(r2) #OS function getr and put it in python
-    print("asm out:", outa)
-    tot = (tot+5) if outp == outa else tot
     
+    # python output
+    outp = Abs(testcase) #python output
+    
+    # assembly output
+    setr(r1, testcase) # r1 = testcase
+    start(absf) #assembly function
+    outa = getr(r2) #OS function getr and put it in python
+    
+    # compare assembly to python
+    print("\nTest Case 1: {}\nPython out: {}\nAssembly out\nTest Grade: {}\n".format(testcase, outp, outa, tot))
+    tot = (tot+5) if outp == outa else tot # if out python == out assembly, increase total, else don't.
+    
+    ## test case 2
     testcase = 5
     outp = Abs(testcase)
-    print("py our: ", outp)
+    
+    # assembly output
     setr(r1, testcase)
     start(absf)
-    # printr(r2)
     outa = getr(r2) #OS function getr and put it in python
-    print("asm out:", outa)
+    
+    # compare assembly to python
     tot = (tot+5) if outp == outa else tot
-    
-    print("test grade: ", tot)
-    
+    print("\nTest Case 2: {}\nPython out: {}\nAssembly out: {}\nTest Grade: {}\n".format(testcase, outp, outa, tot))
