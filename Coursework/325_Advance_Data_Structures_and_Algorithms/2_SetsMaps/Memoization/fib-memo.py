05d94a09124ff3ba7bdc8b4160d68757efc80688
Sunzid Hassan <sunzidhassan@gmail.com>
2023-03-29 12:46:22 -0500

    fibo memo update

--- Coursework/325_Advance_Data_Structures_and_Algorithms/2_SetsMaps/Memoization/fib-memo.py
+++ Coursework/325_Advance_Data_Structures_and_Algorithms/2_SetsMaps/Memoization/fib-memo.py
@@ -1,47 +1,27 @@
 from hashMap import HashMap
 
 # create HashMap to store calculated values
-
 f = HashMap()
-
-
-# fib(n) = fib(n-1) + fib(n-2)
-
-# fib(5) = fib(4)+ fib(3)
-#         = fib(3)+ fib(2) fib(2) + fib(1)
-#         = fib(2)+ fib(1) fib(1) + fib(0) + fib(1)+ fib(0) + fib(1)
-
-# if fib(n) in hashmap, return fib(n)
-# if fib(0) return 0
-# if fib(1) return 1
-# if fib(n) = fib(n-1) + fib(n-2) 
-
-# fib(0) = 0
 
 def fib(n):
     # if already calculated for n, then retrieve result from HashMap and return it
-    if f["n"] != None:
-        fib(n) = f["n"]
-        return f["n"]
-
+    if n in f:
+        return f[n]
     # if calculating for 0, then record result to HahsMap and return result
     if n == 0:
-      f["n"] = 0
-      return 0
-
+        f[n] = 0
+        return f[n]
     # if calculating for 1, then record result to HahsMap and return result
     if n == 1:
-      f["n"] = 1
-      return 1
-    
-    # if calculating for != 0, 1, value that is alredy in HashMap, then
-    else:
-        f["n"] = fib(n-1) + fib(n-2)        
-        return f["n"]
+        f[n] = 1
+        return f[n]
+    # if calculating for != 0, 1, value that is alredy in HashMap, then calculate value according to Fib formula, record the result to HashMap and return the result
+    elif n > 1:
+        value = fib(n-1) + fib(n-2)
+        f[n] = value
+        return f[n]
 
-    
-    # calculate value according to Fib formula, record the result to HashMap
-    # and return the result
+# used idea from: <https://towardsdatascience.com/memoization-in-python-57c0a738179a>
 
 def main():
     print(fib(100))
