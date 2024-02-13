6146439f1f51925434bcd10c619926107fe535ea
Sunzid <sunzidhassan@gmail.com>
2024-02-13 17:27:51 -0600

    rename

--- LaTech/521_Computer_Architecture/521Final/partBq/bcpufeq0.py
+++
@@ -1,8 +0,0 @@
-# don't change the file name, this file cannot contain syntax error
-from bcpu import *
-
-# provide your info:
-myName = 'Hassan, Sunzid' 
-myTechID = '10407862' # CWID
-myTechEmail = 'sha040' # omit @latech.edu
-
--- LaTech/521_Computer_Architecture/Mid/bcpu5mtbq/bcpu3mtbq/bcpumtq0.py
+++
@@ -1,4 +0,0 @@
-# don't change the file name, this file cannot contain syntax error
-myName = 'Hassan, Sunzid'
-myTechID = '10407862' # CWI
-myTechEmail = 'sha040' # omit @latech.edu
